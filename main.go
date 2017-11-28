b0fed62a3e8ce9ea5e8877749b1d40603fda97de
adam lounds <adamlounds@gmail.com>
2017-11-28 21:01:34 +0000

    Add telnet server to sit alongside HTTP one

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	//"fmt"
+	"fmt"
+	"github.com/firstrow/tcp_server"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/pressly/lg"
@@ -17,6 +18,9 @@ const (
 )
 
 var count = 0
+var numActiveSessions = 0
+var numConnections = 0
+var numLinesReceived = 0
 var top5Words = [5]string{"lorem", "ipsum", "dolor", "sit", "amet"}
 var top5Letters = [5]string{"e", "t", "a", "o", "i"}
 var serverCtx context.Context
@@ -31,8 +35,27 @@ func main() {
 	lg.Log(serverCtx).Infof("Starting Innocuous server %s", "v1.0")
 
 	startHTTPServer(defaultHTTPport)
+	startTelnetServer(defaultTelnetPort)
 	select {}
+}
 
+func startTelnetServer(TelnetPort string) {
+	server := tcp_server.New(TelnetPort)
+
+	server.OnNewClient(func(c *tcp_server.Client) {
+		numConnections++
+		numActiveSessions++
+		c.Send(fmt.Sprintf("Hello. You are connection %d\n", numActiveSessions))
+	})
+	server.OnNewMessage(func(c *tcp_server.Client, message string) {
+		numLinesReceived++
+	})
+	server.OnClientConnectionClosed(func(c *tcp_server.Client, err error) {
+		numActiveSessions--
+	})
+
+	go server.Listen()
+	lg.Log(serverCtx).Infof("Started telnet server on port %s", TelnetPort)
 }
 
 func startHTTPServer(HTTPPort string) {
@@ -41,8 +64,8 @@ func startHTTPServer(HTTPPort string) {
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello world"))
-		s := strconv.Itoa(count)
-		w.Write([]byte(s))
+		w.Write([]byte(strconv.Itoa(numConnections)))
+		w.Write([]byte(strconv.Itoa(numLinesReceived)))
 	})
 
 	go func() {
--- main_test.go
+++ main_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"github.com/pressly/lg"
 	"github.com/sirupsen/logrus"
 	. "github.com/smartystreets/goconvey/convey"
 	"golang.org/x/net/context"
+	"net"
 	"testing"
+	"time"
 )
 
 func TestHTTPServer(t *testing.T) {
@@ -19,7 +22,46 @@ func TestHTTPServer(t *testing.T) {
 	lg.Log(serverCtx).Infof("Starting Innocuous server %s", "v1.0")
 
 	Convey("Innocuous server compiles", t, func() {
-		So(func() { startHTTPServer(":9099") }, ShouldNotPanic)
+		So(func() { startHTTPServer(":9098") }, ShouldNotPanic)
 
 	})
+}
+
+func TestTelnetServer(t *testing.T) {
+
+	logger := logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{}
+	lg.RedirectStdlogOutput(logger)
+	lg.DefaultLogger = logger
+	serverCtx = context.Background()
+	serverCtx = lg.WithLoggerContext(serverCtx, logger)
+	lg.Log(serverCtx).Infof("Starting Innocuous server %s", "v1.0")
+
+	Convey("Innocuous telnet server can be started", t, func() {
+		So(func() { startTelnetServer(":9099") }, ShouldNotPanic)
+	})
+
+	// wait for server to wake up
+	time.Sleep(10 * time.Millisecond)
+
+	var conn net.Conn
+	var err error
+	Convey("Can connect to server", t, func() {
+		conn, err = net.Dial("tcp", "localhost:9099")
+		lg.Log(serverCtx).Infof("connected %#V", conn)
+		So(err, ShouldEqual, nil)
+	})
+
+	Convey("..and it responds with a welcome message", t, func() {
+		reply := make([]byte, 1024)
+		_, err = conn.Read(reply)
+
+		// this one was fun :-)
+		reply = bytes.Trim(reply, "\x00")
+		So(err, ShouldEqual, nil)
+		So(string(reply), ShouldEqual, "Hello. You are connection 1\n")
+	})
+
+	conn.Write([]byte("Test message\n"))
+
 }
